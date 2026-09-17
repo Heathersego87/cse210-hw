2c5393a03c0167530482e742a47bfa692371c9fb
heathersego87 <heathersego87@yahoo.com>
2026-09-17 14:38:52 -0500

    Completed C# Prep 2 grade conditions

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string input = Console.ReadLine();
+        int gradePercentage = int.Parse(input);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (gradePercentage >=70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (gradePercentage >=60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
