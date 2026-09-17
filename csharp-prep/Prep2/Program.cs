759e3846457220d77c0906d0fb3ff29d3f17e38c
heathersego87 <heathersego87@yahoo.com>
2026-09-17 15:09:33 -0500

    Added letter variable and pass/fail logic

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -8,25 +8,58 @@ class Program
         string input = Console.ReadLine();
         int gradePercentage = int.Parse(input);
 
+        string letter;
+
         if (gradePercentage >= 90)
         {
-            Console.WriteLine("A");
+            letter = "A";
         }
         else if (gradePercentage >= 80)
         {
-            Console.WriteLine("B");
+            letter = "B";
         }
         else if (gradePercentage >=70)
         {
-            Console.WriteLine("C");
+            letter = "C";
         }
         else if (gradePercentage >=60)
         {
-            Console.WriteLine("D");
+            letter = "D";
         }
         else
         {
-            Console.WriteLine("F");
+            letter = "F";
+        }
+
+        Console.WriteLine(letter);
+
+        if (gradePercentage >=70)
+        {
+            Console.WriteLine("Congratulations! You're passing the class!");
+        }
+        else
+        {
+            Console.WriteLine("Try harder next time!");
+        }
+        if (gradePercentage >=90)
+        {
+            Console.WriteLine("Excellent!");
+        }
+        else if (gradePercentage >=80)
+        {
+            Console.WriteLine("Great Job!");
+        }
+        else if (gradePercentage >=70)
+        {
+            Console.WriteLine("Good Work!");
+        }
+        else if (gradePercentage >=60)
+        {
+            Console.WriteLine("Not your best work!");
+        }
+        else
+        {
+            Console.WriteLine("Dont give up!");
         }
     }
 }
