e82e4fdbc4f79619231b743902ea38eab2585472
heathersego87 <heathersego87@yahoo.com>
2026-09-18 13:19:39 -0500

    Added Prep 3 guessing logic

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.WriteLine("What is the magic number?");
+        string magicNumber = Console.ReadLine();
+        int firstNumber =  int.Parse(magicNumber);
+
+        Console.Write("What is your guess? ");
+        string guess = Console.ReadLine();
+        int firstGuess =  int.Parse(guess);
+
+        
+        if (firstGuess < firstNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (firstGuess > firstNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.Write("You guessed it!");
+        }
     }
 }
