c89677387800e5e1a0b3f1015c6372f8b8f88f95
heathersego87 <heathersego87@yahoo.com>
2026-09-21 14:36:28 -0500

    Complete Resume class and display job history

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -8,18 +8,25 @@ class Program
         Job job1 = new Job();
 
         job1._company = "Microsoft";
-        job1._jobTitle = "Software Developer";
+        job1._jobTitle = "Project Manager";
         job1._startYear = 2020;
         job1._endYear = 2023;
 
         Job job2 = new Job();
 
         job2._company = "Boeing";
-        job2._jobTitle = "Project Manager";
+        job2._jobTitle = "Software Developer";
         job2._startYear = 2023;
         job2._endYear = 2026;
 
-        job1.Display();
-        job2.Display();
+        Resume myResume = new Resume();
+
+        myResume._name = "Heather Sego";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
     }
 }
