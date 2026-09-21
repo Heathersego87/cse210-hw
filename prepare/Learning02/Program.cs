2eefc5e79b50c0d3462dd84422f547de2c6efa07
heathersego87 <heathersego87@yahoo.com>
2026-09-21 14:11:44 -0500

    Add Job class and job details for Learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,25 @@
 using System;
+//Heather Sego
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Developer";
+        job1._startYear = 2020;
+        job1._endYear = 2023;
+
+        Job job2 = new Job();
+
+        job2._company = "Boeing";
+        job2._jobTitle = "Project Manager";
+        job2._startYear = 2023;
+        job2._endYear = 2026;
+
+        job1.Display();
+        job2.Display();
     }
 }
