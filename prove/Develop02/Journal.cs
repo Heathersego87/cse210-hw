e6e84a3b64a4c9ed68cd721d0d2f0fe1aa25ab72
heathersego87 <heathersego87@yahoo.com>
2026-09-24 10:56:13 -0500

    Add journal menu and write entry functionality

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -10,7 +10,7 @@ public class Journal
             Console.WriteLine();
         }
     }
-    public void AddEdntry(Entry newEntry)
+    public void AddEntry(Entry newEntry)
     {
         _entries.Add(newEntry);
     }
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Journal journal = new Journal();
+        List<string> prompts = new List<string>
+        {
+            "How did my day begin?",
+            "What has been the most fulfilling part of my day thus far?",
+            "How have I felt the Lord in my life today?",
+            "What blessings have I been counting?",
+            "Have my thoughts been negative or positive?",
+            "Have I used the pattern interuptions methods to stay happy?"
+        };
+        string choice = "";
+        while (choice!= "5")
+        {
+            Console.WriteLine("Please choose one of the following options:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.WriteLine("What would you like to do?");
+            choice = Console.ReadLine();
+            
+            if (choice == "1")
+            {
+                Random random = new Random();
+                int index = random.Next(prompts.Count);
+
+                string prompt = prompts[index];
+
+                Console.WriteLine(prompt);
+                Console.Write(">");
+
+                string response = Console.ReadLine();
+                DateTime currentDate = DateTime.Now;
+                string date = currentDate.ToShortDateString();
+                Entry newEntry = new Entry();
+                newEntry._date = date;
+                newEntry._promptText = prompt;
+                newEntry._entryText = response;
+                journal.AddEntry(newEntry);
+            }
+        }
     }
 }
